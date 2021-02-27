00ee3cf49465a4c694b8f9f802e972ceb2d9ef5f
Артём Володин <artemvolodin900000@gmail.com>
2021-02-27 17:43:02 +0300

    Added change status function

--- OfficeEquipmentManager/MainResourses/EquipmentEditWindow.xaml.cs
+++ OfficeEquipmentManager/MainResourses/EquipmentEditWindow.xaml.cs
@@ -21,9 +21,12 @@ namespace OfficeEquipmentManager.MainResourses
     /// </summary>
     public partial class EquipmentEditWindow : Window
     {
+        Equipment currentEquipment;
         public EquipmentEditWindow(Equipment currentEquipment)
         {
             InitializeComponent();
+
+            this.currentEquipment = currentEquipment;
 
             DataContext = currentEquipment;
 
@@ -52,7 +55,8 @@ namespace OfficeEquipmentManager.MainResourses
         Grid grid;
         private void GetStatusVisuals()
         {
-            for (int i = 0; i < ContextConnector.db.EquipmentStatus.Count(); i++)
+            statusPanel.Children.Clear();
+            for (int i = 0; i < currentEquipment.EquipmentStatus.Id; i++)
             {
                 List<EquipmentStatus> equipmentStatuses = ContextConnector.db.EquipmentStatus.ToList();
                 Ellipse ellipse = new Ellipse
@@ -72,7 +76,7 @@ namespace OfficeEquipmentManager.MainResourses
 
                 block.VerticalAlignment = VerticalAlignment.Center;
                 block.HorizontalAlignment = HorizontalAlignment.Center;
-                block.Margin = new Thickness(-25, 0, 0, 0);
+                block.Margin = new Thickness(-30, 0, 0, 0);
 
                 grid.Children.Add(ellipse);
                 grid.Children.Add(block);
@@ -83,20 +87,24 @@ namespace OfficeEquipmentManager.MainResourses
         }
 
             void ConnectTheEllipses()
-            {   
-                for (int i = 0; i < ellipses.Count; i+=2)
-                {
-                    double left = ellipses[i].RenderedGeometry.Bounds.Left;
-                    double right = ellipses[i++].RenderedGeometry.Bounds.Right;
-                    Line line = new Line
-                    {
-                        X1 = left,
-                        Y1 = right,
-                        StrokeThickness = 2,
-                        Stroke = Brushes.Black
-                    };
-                    grid.Children.Add(line);
-                }
+            {
+                line.Visibility = Visibility.Visible;
             }
+
+        private void changeEquipmentStatus_MouseEnter(object sender, MouseEventArgs e)
+        {
+            listBoxStatuses.ItemsSource = ContextConnector.db.EquipmentStatus.ToList();
+            popUp.IsOpen = true;
+        }
+
+        private void listBoxStatuses_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            currentEquipment.EquipmentStatus = listBoxStatuses.SelectedItem as EquipmentStatus;
+            ContextConnector.db.SaveChanges();
+            MessageBox.Show("Статус успешно изменен","Успех",MessageBoxButton.OK,MessageBoxImage.Information);
+            GetStatusVisuals();
+            popUp.IsOpen = false;
         }
     }
+}
+ 
